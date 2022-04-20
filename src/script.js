85aea608b7eb734ddf8ed479204ef1b7b283d10e
Javier R <javierresines@gmail.com>
2022-04-20 18:30:59 -0500

    fixed API issue

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ var button = document.querySelector('#locationbtn');
 
 function informationfunction() {
 
-fetch ('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&&appid=ae90054c5cfbcc338314940f67ed4e1eunits=metric')
+fetch ('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=ae90054c5cfbcc338314940f67ed4e1e&units=metric')
 .then(response => response.json())
 .then(data => console.log(data));
 

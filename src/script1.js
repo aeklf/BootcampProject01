34dae48d37cd8543d296f7f6698f9e1f5dfee280
Daniel Olmedo <olmedo.daniel96@gmail.com>
2022-04-26 17:43:54 -0500

    General corrections

--- src/script1.js
+++ src/script1.js
@@ -1,59 +1,16 @@
 //Test script for back end logic//
 
-// API (test) fetch function:  returns variables to be used in outfit assignment function.
-
-var button = document.querySelector('#locationbtn');
-var temperatureValue;
-var humidityValue;
-var input = document.querySelector('#location');
-
-// updated extracts location from user input 
-
-function informationfunction() {
-
-    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=ae90054c5cfbcc338314940f67ed4e1e&units=metric')
-    .then(response => response.json())
-    .then(data => {
-        temperatureValue = data['main']['temp'];
-        humidityValue = data ['main']['humidity'];
-        DescriptionValue = data['weather'][0]['description'];
-        NameValue = data['name'];
-        console.log("(fetch primary level)Temperature (ºC): " + temperatureValue);
-        console.log("(fetch primary level))Description: " + DescriptionValue);
-        console.log("(fetch primary level)Humidity (%): " + humidityValue); 
-        console.log("(fetch primary level)Name: " + NameValue);
-        currentWeather(temperatureValue,humidityValue);  
-          
-    }).catch(error  => {
-    console.log(error);
-})
-    
-    console.log("Test in fetch (secondary level)");
-    returnFun();  
-    
-}
-
-// Button: runs main fetch function. 
-
-button.addEventListener('click', informationfunction);
-
-// Function to return humidity in 0-1 value.
-
-function returnFun(){
-    console.log("Test in nested function. T(ºC): " + temperatureValue + " Humidity(%): " + humidityValue);
-    return temperatureValue, (humidityValue/100);
-}
-
-
+localStorage.getItem('User1');
+console.log(User1);
 
 //Class creation to assign wardrobe as parameters and methods as functions for oufit assignment (template for user objects)
 class NewUser {
     constructor(first, last, email, location, accessories, top, bottom, shoes) {
 
-        this.first = first;
-        this.last = last;
-        this.email = email;
-        this.location = location;
+        // this.first = first;
+        // this.last = last;
+        // this.email = email;
+        // this.location = location;
         this.accessories = accessories;
         this.top = top;
         this.bottom = bottom;
@@ -61,13 +18,13 @@ class NewUser {
       
     }
 
-    isValid() {
-        if(this.first === "" || this.last === "" || this.email === "" || this.location === ""){
-            return false
-        } else {
-            return true
-        }
-    }
+    // isValid() {
+    //     if(this.first === "" || this.last === "" || this.email === "" || this.location === ""){
+    //         return false
+    //     } else {
+    //         return true
+    //     }
+    // }
 
     assignOutfit1(){
         $('.outfit').empty();
@@ -120,7 +77,7 @@ class NewUser {
         $('#topText').html(this.accessories.option2)
         $('#bottomText').html(this.top.option2);
         $('#shoesText').html(this.bottom.option2);
-
+        $('outfit').append(image1)
     }
     assignOutfit7(){
         $('.outfit').empty();
@@ -164,10 +121,57 @@ class NewUser {
 var User1Jackets = {option1:"hoodie", option2:"T-shirt"};
 var User1Pants = {option1:"jeans", option2:"shorts"};
 var User1Glasses = {option1:"sunglasses", option2:"cap"};
+var User1Shoes = {option1:"sneakers", option2:"sandals"};
 
 // Test creation for "User" instance.
 
-const User1 = new User(Object.assign(User1Jackets),Object.assign(User1Pants),Object.assign(User1Glasses));
+const User1 = new NewUser(Object.assign(User1Jackets),Object.assign(User1Pants),Object.assign(User1Glasses),Object.assign(User1Shoes));
+
+// API (test) fetch function:  returns variables to be used in outfit assignment function.
+
+var button = document.querySelector('#locationbtn');
+var temperatureValue;
+var humidityValue;
+var input = document.querySelector('#location');
+
+// updated extracts location from user input 
+
+function informationfunction() {
+
+    fetch ('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=ae90054c5cfbcc338314940f67ed4e1e&units=metric')
+    .then(response => response.json())
+    .then(data => {
+        const User1 = new NewUser(Object.assign(User1Jackets),Object.assign(User1Pants),Object.assign(User1Glasses),Object.assign(User1Shoes));
+        console.log(User1)
+        temperatureValue = data['main']['temp'];
+        humidityValue = data ['main']['humidity'];
+        DescriptionValue = data['weather'][0]['description'];
+        NameValue = data['name'];
+        console.log("(fetch primary level)Temperature (ºC): " + temperatureValue);
+        console.log("(fetch primary level))Description: " + DescriptionValue);
+        console.log("(fetch primary level)Humidity (%): " + humidityValue); 
+        console.log("(fetch primary level)Name: " + NameValue);
+        currentWeather(temperatureValue,humidityValue);  
+          
+    }).catch(error  => {
+    console.log(error);
+})
+    
+    console.log("Test in fetch (secondary level)");
+    returnFun();  
+    
+}
+
+// Button: runs main fetch function. 
+
+button.addEventListener('click', informationfunction);
+
+// Function to return humidity in 0-1 value.
+
+function returnFun(){
+    console.log("Test in nested function. T(ºC): " + temperatureValue + " Humidity(%): " + humidityValue);
+    return temperatureValue, (humidityValue/100);
+}
 
 //Function for interpreting variables from API fetch, used for assignment of wardrobe options.
 
@@ -180,7 +184,7 @@ function currentWeather(temperatureValue,humidityValue){
 
     if(6 < temperatureValue && 13 >= temperatureValue && humidityValue <= 0.25){
         console.log("Scenario 1")
-        User1.assignJacket1();
+        //User1.assignOutfit1();
         // User1.assignOutfit1();
     }
 
@@ -233,7 +237,7 @@ function currentWeather(temperatureValue,humidityValue){
     }
     else {
         console.log("Scenario 10,11 or 12")
-        User1.assignJacket1();
+        //User1.assignOutfit1();
         // User1.assignOutfit10();
     }
 }
